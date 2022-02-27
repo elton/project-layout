8cafd46034c931ebdcee26e199bbba1715e1cf81
Elton Zheng <elton.zheng.sh@gmail.com>
2022-02-27 17:40:43 +0800

    add air to live reload for go app.

---
+++ app/myapp/scripts/dev.sh
@@ -0,0 +1,9 @@
+# Copyright (c) 2022 Elton Zheng
+# 
+# This software is released under the MIT License.
+# https://opensource.org/licenses/MIT
+
+#!/bin/bash
+
+
+APP_ENV=development go run github.com/elton/project-layout/app/myapp/cmd/myapp
